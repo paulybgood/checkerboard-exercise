775f3430904f92314e28cc7193ad03e92f11a062
Paul Bialas <Paulybgood@users-MacBook-Pro.local>
2021-09-03 09:21:04 -0500

    Paul's checkerboard solution

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 // Your JS goes here
+
+for (let i = 1; i <= 8; i++) {
+    if (i % 2 === 0) {
+        for (let j = 0; j <= 7; j++) {
+            const div = document.createElement("div");
+            document.body.style.width = "100vh";
+            document.body.style.height = "100vh";
+            div.style.width = "12.5%";
+            div.style.height = "12.5%";
+            div.style.float = "left";
+            document.body.appendChild(div);
+            if (j % 2 === 0) {
+                div.style.backgroundColor = "red";
+            
+            } else {
+                div.style.backgroundColor = "black";
+            }
+        }
+    } else {
+        for (let j = 0; j <= 7; j++) {
+            const div = document.createElement("div");
+            document.body.style.width = "100vh";
+            document.body.style.height = "100vh";
+            div.style.width = "12.5%";
+            div.style.height = "12.5%";
+            div.style.float = "left";
+            document.body.appendChild(div);
+            if (j % 2 === 1) {
+                div.style.backgroundColor = "red";
+            
+            } else {
+                div.style.backgroundColor = "black";
+            }
+        }
+    }
+}
